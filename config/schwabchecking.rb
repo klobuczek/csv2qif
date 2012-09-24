34dc1de340b479690d2f535fecfa63335d0c5fd7
Heinrich Klobuczek <heinrich@mail.com>
2012-09-23 17:35:34 -0700

    minor changes to bundle

--- config/schwabchecking.rb
+++ config/schwabchecking.rb
@@ -1,7 +1,7 @@
 {
         :type => 'Bank',
         :header => 4,
-        #:date_format => "%m/%d/%Y",
+        :date_format => "%m/%d/%y",
         :date => lambda { a },
         :payee => lambda { d },
         :amount => lambda { e && !e.empty? ? -e.to_f : f },
